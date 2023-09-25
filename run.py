8e11de2206751936f9531936caeeaff657aefb2b
Caramelsprinkle <jm.hartono@gmail.com>
2023-09-25 22:10:47 +0700

    Transitioning to subclassing attempt 2

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ from cogs.pingpong import PingPong
 from config import TOKEN
 
 def main():
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+    
     bot = CustomBotClient(
         command_prefix=">>>",
         intents=intents
@@ -14,30 +18,8 @@ def main():
     
     bot.run(TOKEN)
 
+    handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+    bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+
 if __name__ == "__main__":
     main()
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-class CustomClient(discord.Client(intents=intents)):
-    async def on_ready(self):
-        print(f"{self.user} is ready!")
-    
-    async def on_message(self, msg):
-        msgChannel = msg.channel
-        msgContent = msg.content
-        msgAuthor = msg.author
-    
-        if msgAuthor == self.user:
-            return
-    
-        if msgContent == "!ping":
-            await msgChannel.send(f"Pong!\n{round(client.latency * 1000)}ms")
-        
-client = CustomClient
-        
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
-
-client.run(TOKEN)
