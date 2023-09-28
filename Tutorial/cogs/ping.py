afcf4b26fb3af657cffaacfa666c7d8349e96134
Caramelsprinkle <jm.hartono@gmail.com>
2023-09-28 14:55:59 +0700

    Added MagicBall as a cog

--- Tutorial/cogs/ping.py
+++ Tutorial/cogs/ping.py
@@ -5,9 +5,9 @@ class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.Cog.listener() # events must use commands.Cog.listener
-    async def on_ready(self):
-        print("Ping.py is ready!")
+    # @commands.Cog.listener() # events must use commands.Cog.listener
+    # async def on_ready(self):
+    #     print("Ping.py is ready!")
         
     @commands.command() # commands must use commands.command
     async def ping(self, ctx): # According to OOP rules, you must always pass the object first before other parameters for commands
--- Tutorial/tutorialbot.py
+++ Tutorial/tutorialbot.py
@@ -27,21 +27,6 @@ async def on_ready(): #starts when the bot starts running
     print("Success: Bot is ready to use!")
     change_status.start() # gives the bot an initial status on startup
 
-# @bot.command() # requires user to type in !ping for the bot to return "Pong!" and latency
-# async def ping(ctx): # 
-#     # stores the bot's latency into a variable, scaled to provide latency in ms 
-#     botLatency = round(bot.latency * 1000)
-#     await ctx.send(f"Pong!\n{botLatency} ms")
-    
-@bot.command(aliases=("8ball", "8b", "magicball")) # alias provides different alternatives for how the command can be referred to in Discord
-async def magic_eightball(ctx, *, question): # * allows for multiple, unknown parameters to be used 
-    # opens the text file "eightball_responses.txt" and interprets it as a list[] in random_responses
-    with open("Tutorial/eightball_responses.txt", "r") as magicBallLines:
-        random_responses = magicBallLines.readlines()
-        response = random.choice(random_responses) # outputs a random string from the list of responses
-    
-    await ctx.send(response) # the bot sends the randomized response
-
 # giving the bot a status
 @tasks.loop(seconds=5) # function occurs at an interval of every 5 seconds
 async def change_status():
@@ -62,8 +47,5 @@ async def main():
         await bot.start(TOKEN)
         
 asyncio.run(main()) # runs the main function
-
-# bot.run is no longer necessary with main()
-# bot.run(TOKEN)
 
 #Tutorial: https://www.youtube.com/watch?v=x7oBQNcNGeM&list=PLwqYQaS6jxfk_NCetUOyNRDGAf9_kU90n&index=1&pp=iAQB
