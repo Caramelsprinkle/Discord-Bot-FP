fa9748571ef596714e56dcb1d5ba1b7a0152d502
Caramelsprinkle <jm.hartono@gmail.com>
2023-09-28 14:08:16 +0700

    FInished episode 3 of tutorial

--- Tutorial/tutorialbot.py
+++ Tutorial/tutorialbot.py
@@ -1,5 +1,6 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
 from config import TOKEN
 import random
 
@@ -7,19 +8,29 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-client = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+# status shows up under the bot's username. NEEDS to be a list
+clientStatus = cycle(["in 30",
+                      "in 25",
+                      "in 20",
+                      "in 15",
+                      "in 10",
+                      "in 5",
+                      "Error!!!",
+                      "rebooting.."])
 
-@client.event
+@bot.event
 async def on_ready(): #starts when the bot starts running
     print("Success: Bot is ready to use!")
+    change_status.start() # gives the bot an initial status on startup
 
-@client.command() # requires user to type in !ping for the bot to return "Pong!" and latency
+@bot.command() # requires user to type in !ping for the bot to return "Pong!" and latency
 async def ping(ctx): # 
     # stores the bot's latency into a variable, scaled to provide latency in ms 
-    botLatency = round(client.latency * 1000)
+    botLatency = round(bot.latency * 1000)
     await ctx.send(f"Pong!\n{botLatency} ms")
     
-@client.command(aliases=("8ball", "8b", "magicball")) # alias provides different alternatives for how the command can be referred to in Discord
+@bot.command(aliases=("8ball", "8b", "magicball")) # alias provides different alternatives for how the command can be referred to in Discord
 async def magic_eightball(ctx, *, question): # * allows for multiple, unknown parameters to be used 
     # opens the text file "eightball_responses.txt" and interprets it as a list[] in random_responses
     with open("Tutorial/eightball_responses.txt", "r") as magicBallLines:
@@ -28,6 +39,11 @@ async def magic_eightball(ctx, *, question): # * allows for multiple, unknown pa
     
     await ctx.send(response) # the bot sends the randomized response
 
-client.run(TOKEN) # command gets run by the bot with the associated token
+# giving the bot a status
+@tasks.loop(seconds=5) # function occurs at an interval of every 5 seconds
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(clientStatus))) # changes the Discord bot's status
+
+bot.run(TOKEN) # command gets run by the bot with the associated token
 
 #Tutorial: https://www.youtube.com/watch?v=x7oBQNcNGeM&list=PLwqYQaS6jxfk_NCetUOyNRDGAf9_kU90n&index=1&pp=iAQB
